9c21ebe8f8af93f66f3507b25cb803725f2837ed
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-03-08 00:45:57 +0900

    step1

---
+++ step0-1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## gitレポジトリのクローン
+
+# :large_orange_diamond: Action: ターミナルで以下の一連のコマンドを実行してください
+
+```
+git clone https://github.com/richardimaoka/apollo-server-tutorial.git
+cd apollo-server-tutorial
+```
